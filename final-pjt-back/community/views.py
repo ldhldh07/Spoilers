ad89625e000622a274ca5b9f43f7295329589d1d
CHANBIN KIM <driculer@gmail.com>
2022-11-17 12:19:57 +0900

    백앤드

--- final-pjt-back/community/views.py
+++ final-pjt-back/community/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Comment
+from movies.models import Movie
+from .serializers import CommentSerializer
+
 
 # Create your views here.
+@api_view(['POST'])
+def create_comment(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serialzer = CommentSerializer(data=request.data)
+    if serialzer.is_valid(raise_exception=True):
+        # serialzer.save(movie=movie, user=request.user)
+        serialzer.save(movie=movie)
+        return Response(serialzer.data, status=status.HTTP_201_CREATED)
